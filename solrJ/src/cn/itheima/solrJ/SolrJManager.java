2a28f86c3f2b45d753711d1870da1ef48f407ba6
flyas <1479347752@qq.com>
2017-05-26 12:02:48 +0800

    改变了一点地方

--- solrJ/src/cn/itheima/solrJ/SolrJManager.java
+++ solrJ/src/cn/itheima/solrJ/SolrJManager.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 public class SolrJManager {
     
 	public static void main(String[] args) {
-		System.out.println("git");
+		System.out.println("git！！！");
 	}
 	/**
 	 * 添加
